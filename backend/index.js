cd46b2d791d3280d44314efa54ee05b46f445234
Diyora-Jeel <jeeldiyora101@gmail.com>
2025-02-10 21:51:57 -0800

    change cors origin

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 app.use(express.json())
 app.use(express.urlencoded())
 app.use(cors({
-  origin : process.env.FRONTEND_URL,
+  origin : "*",
   credentials : true,
   methods : "GET,POST,PUT,DELETE",
   allowedHeaders:["Content-Type","Authorization"]
